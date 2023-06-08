094c6c88c1493935b1cb5d0281acdf3fd1c801e9
Mohamed fareed <131618380+mfc0d1ng@users.noreply.github.com>
2023-06-08 10:30:25 +0300

    Update make.h

--- make/make.h
+++ make/make.h
@@ -9,8 +9,6 @@ extern std::string create_map_dir(void);
 
 extern void open_map_file(const char *argv1, FILE **map, const char *__dirPtr);
 
-extern void clean_up(const char *__ptr, FILE **map);
-
 extern void clean_up(FILE **map);
 
 extern std::size_t get_map_file(std::string &_file_content, FILE **map);
